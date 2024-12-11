c1d183bf575a09bb3dc60714c825db5a94ff57ee
IvanSolodilov <82935452+IvanSolodilov@users.noreply.github.com>
2024-12-12 02:16:05 +0300

    Create componentDZ.vue

---
+++ componentDZ.vue
@@ -0,0 +1,34 @@
+// В App 
+
+import Mylist from './components/MyList.vue';
+const listItem = ["item1", "item2", "item3"];
+
+<Mylist :list="listItem" />
+
+
+
+// В MyList
+
+
+<script setup>
+
+// макрос defineProps используется внутри script setup
+
+defineProps({
+    list: {
+        type: Array,
+        required: true,
+    },
+})
+
+</script>
+
+<template>
+
+    <ul>
+        <li v-for="item in list"> {{ item }}</li>
+    </ul>
+
+</template>
+
+
